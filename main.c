31922de22d35d37c92289ffe05f2f314ecaed5f4
Shripada Hebbar <shripada.hebbar@codecraft.co.in>
2022-09-24 05:26:36 +0530

    basic prime number implementation

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int main() {
+    printf("Please enter the number\n");
+    int n;
+    scanf("%d", &n);
+    int flag = 1; 
+    for(int i=2; i<= n/2; i++){
+        if( n % i == 0){
+            flag = 0;
+            break;
+        }
+    }
+    if(flag == 1){
+        printf("%d is a prime number\n", n);
+    }else {
+         printf("%d is not a prime number\n", n);
+    }
+    return 0;
+}
